e5ecfe3114c8f7fa5a824cfa2318a398196b5d5a
Kamal Krishnamurthy <kamal@paywize.in>
2026-02-13 12:31:48 +0530

    added useragent

--- api-tests/utils/FintechApiHelper.js
+++ api-tests/utils/FintechApiHelper.js
@@ -40,6 +40,7 @@ class FintechApiHelper {
           otp: credentials.otp,
         },
         headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
           'Content-Type': 'application/json',
           'Accept': 'application/json',
         },
